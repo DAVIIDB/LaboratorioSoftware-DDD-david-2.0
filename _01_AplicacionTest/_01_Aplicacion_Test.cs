1018a10022ca76d712af816c8001de30a0864d73
DAVIIDB <48736357+DAVIIDB@users.noreply.github.com>
2020-07-06 15:20:00 -0300

    Commit 1.1
    Primera parte de Prueba Unitaria Aplicaion_Test

---
+++ _01_AplicacionTest/_01_Aplicacion_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_AplicacionTest
+{
+    [TestClass]
+    public class Aplicacion_Test
+    {
+        _03_InfraestructuraDatos.SQLServer.TipoProductoRepositorio r = new _03_InfraestructuraDatos.SQLServer.TipoProductoRepositorio();
+        [TestMethod]
+        public void Prueba_DatosNoValidos()
+        {
+            //ANT 
+            int i =0;
+            
+            _01_Aplicacion.TipoProductoServicio tps = new _01_Aplicacion.TipoProductoServicio(r);
+            //_02_Dominio.Entidades.TipoProducto tp = new _02_Dominio.Entidades.TipoProducto();
+            _02_Dominio.Entidades.TipoProducto eliminar = tps.Eliminar(i);
+            //ACT y ASERT
+            Assert.AreEqual(eliminar.Id, i);
+
+        }
+    }
+}
